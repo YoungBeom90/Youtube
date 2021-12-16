80a150a798ed554491ffbb70f68620d19ed856df
ybKim <ybkim190831@gmail.com>
2021-12-16 15:02:23 +0900

    ..

--- src/components/video_detail/videoDetail.jsx
+++ src/components/video_detail/videoDetail.jsx
@@ -5,12 +5,13 @@ const VideoDetail = ({video, video: {snippet}}) => (
     <section className={styles.detail}>
         <iframe 
             className={styles.video}
-            type="text/html" 
+            type="text/html"
+            title="youtube player"
             width="100%" 
             height="405"
             src={`https://www.youtube.com/embed/${video.id}`}
-            frameborder="0" 
-            allowfullscreen
+            frameBorder="0" 
+            allowFullScreen
         />
         <h2>{snippet.title}</h2>
         <h3>{snippet.channelTitle}</h3>
